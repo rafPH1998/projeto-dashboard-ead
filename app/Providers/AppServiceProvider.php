637edd6d824d59b25c3ba405ef0daf34d3389e6d
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-08-21 00:02:06 -0300

    att do provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,19 @@
 
 namespace App\Providers;
 
+use App\Repositories\Eloquent\{
+    UserRepository,
+    CourseRepository,
+    ModuleRepository,
+};
+
+use App\Repositories\{
+    UserRepositoryInterface,
+    CourseRepositoryInterface,
+    ModuleRepositoryInterface,
+
+};
+
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +34,11 @@ class AppServiceProvider extends ServiceProvider
         $this->app->singleton(
             CourseRepositoryInterface::class,
             CourseRepository::class,
+        );
+
+        $this->app->singleton(
+            ModuleRepositoryInterface::class,
+            ModuleRepository::class,
         );
     }
 
