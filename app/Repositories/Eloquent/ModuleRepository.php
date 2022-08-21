8181771ba6356a78d3cf0e716df0b80582615ffc
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-08-21 00:03:14 -0300

    repository de modulo

---
+++ app/Repositories/Eloquent/ModuleRepository.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Repositories\Eloquent;
+
+use App\Models\Module;
+use App\Repositories\ModuleRepositoryInterface;
+
+class ModuleRepository implements ModuleRepositoryInterface
+{
+
+    public function __construct(protected Module $modules)
+    {
+
+    }
+
+    public function getAllByCourse(string $courseId, string $filter = ''): array
+    {
+        $modules = $this->modules
+                        ->where(function ($query) use ($filter) {
+                            if ($filter) {
+                                $query->where('email', $filter);
+                                $query->Orwhere('name', 'LIKE', "%{$filter}%");
+                            }
+                        })
+                        ->where('course_id', $courseId)
+                        ->get();
+
+        return $modules->toArray();
+    }
+
+    public function findById(string $id): object|null
+    {
+        return $this->modules->find($id);
+    }
+
+    public function createByCourse(string $courseId, array $data): object
+    {
+        return $this->modules->create($data);
+    }
+
+    public function update(string $id, array $data): object|null
+    {
+        $user = $this->findById($id);
+
+        if (!$user) {
+            return null;
+        }
+
+        $user->update($data);
+
+        return $user;
+    }
+
+    public function delete(string $id): bool
+    {
+        $user = $this->findById($id);
+
+        if (!$user) {
+            return false;
+        }
+
+        return $user->delete();
+    }
+    
+}
+
