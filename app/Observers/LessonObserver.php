8c2b4a48f7a412347991e2b107305eb7d568819e
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-08-22 15:58:08 -0300

    adicionando observer para url

---
+++ app/Observers/LessonObserver.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Observers;
+
+use Illuminate\Support\Str;
+use App\Models\Lesson;
+
+class LessonObserver
+{
+    /**
+     * Handle the User "creating" event.
+     *
+     * @param  \App\Models\Lesson  $user
+     * @return void
+     */
+    public function creating(Lesson $lesson)
+    {
+        $lesson->url = Str::slug($lesson->name);
+    }
+
+    /**
+     * Handle the User "updating" event.
+     *
+     * @param  \App\Models\Lesson  $user
+     * @return void
+     */
+    public function updating(Lesson $lesson)
+    {
+        $lesson->url = Str::slug($lesson->name);
+    }
+}
