aa8f28d49bf2ff251c90cb39da15a31c53ca9048
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-08-23 16:59:02 -0300

    controller de support

---
+++ app/Http/Controllers/SupportController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Repositories\Eloquent\SupportRepository;
+use Illuminate\Http\Request;
+
+class SupportController extends Controller
+{
+
+    public function __construct(protected SupportRepository $support_repository)
+    {
+    }
+
+    public function index(Request $request)
+    {
+        $supports = $this
+                    ->support_repository
+                    ->getSupports($request->get('status', 'P'));
+
+        return view('admin.supports.index', [
+            'supports' => convertItemsOfArrayToObject($supports)
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
