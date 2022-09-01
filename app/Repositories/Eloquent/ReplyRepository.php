138f2883f307031692c3db87cbea46b186bd314c
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-09-01 15:57:43 -0300

    removendo createReplyToSupport do controller

--- app/Repositories/Eloquent/ReplyRepository.php
+++ app/Repositories/Eloquent/ReplyRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repositories\Eloquent;
 
+use App\Events\SupportReplied;
 use App\Models\ReplySupport;
 use App\Repositories\ReplyRepositoryInterface;
 
@@ -11,11 +12,6 @@ class ReplyRepository implements ReplyRepositoryInterface
     public function __construct(
         protected ReplySupport $reply_supports
     ){}
-
-    public function createReplyToSupport(array $data): array
-    {
-        return $this->reply_supports->create($data);
-    }
 
     public function findById(string $id): object|null
     {
