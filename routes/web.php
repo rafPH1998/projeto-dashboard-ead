4b98b5a023c13c55217ece575b1ef6324e9e0551
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-09-01 15:58:39 -0300

    add rota de confiMyAccount

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\{
     UserController,
     CourseController,
+    DashboardController,
     ModuleController,
     LessonController,
     SupportController,
@@ -20,7 +21,6 @@ Route::prefix('/admin')
      * Routes Users
      */
     Route::resource('/users', UserController::class);
-
 
     /**
      * Routes Courses
@@ -48,16 +48,22 @@ Route::prefix('/admin')
      */
     Route::post('/supports/{id}/reply', [ReplySupportController::class, 'store'])->name('replies.store');
 
+    /**
+     * Routes Dashboard Home
+     */
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+
      /**
      * Routes Admins
      */
     Route::get('/', [AdminController::class, 'index'])->name('admin.index');
-    
     Route::get('/create', [AdminController::class, 'create'])->name('admin.create');
     Route::post('/store', [AdminController::class, 'store'])->name('admin.store');
     Route::get('/{admin}/edit', [AdminController::class, 'edit'])->name('admin.edit');
     Route::put('/{admin}/edit', [AdminController::class, 'update'])->name('admin.update');
     Route::delete('/{admin}/destroy', [AdminController::class, 'destroy'])->name('admin.destroy');
+    Route::get('/myCount', [AdminController::class, 'myAccount'])->name('admin.myAccount');
+    Route::put('/myCount', [AdminController::class, 'confiMyAccount'])->name('admin.confiMyAccount');
 });
 
 require __DIR__.'/auth.php';
