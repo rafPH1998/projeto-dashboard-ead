ef0b751f59d91c39046138003a4a5868ee8acb5f
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-09-01 15:56:39 -0300

    removendo contrato createReplyToSupport

--- app/Repositories/ReplyRepositoryInterface.php
+++ app/Repositories/ReplyRepositoryInterface.php
@@ -5,7 +5,5 @@ namespace App\Repositories;
 
 interface ReplyRepositoryInterface
 {
-
-    public function createReplyToSupport(array $data): array;
     public function findById(string $id): ?object;
 }
