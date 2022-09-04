ce0c3543ea3e2304d7850666de5c3f6a0237aa88
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-09-04 18:53:57 -0300

    welcome

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use App\Http\Controllers\{
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+
+    return view('welcome');
+  
+});
+
 Route::prefix('/admin')
     ->middleware(['auth'])
     ->group(function() {
