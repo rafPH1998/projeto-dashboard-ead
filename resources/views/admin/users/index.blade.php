67b4149a727a35a06264c6c329d237da9f11b726
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-09-15 12:43:12 -0300

    add paginação na view usuarios

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -24,25 +24,33 @@
                 <thead>
                     <tr>
                         <th
-                            class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                            class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 
+                                text-left text-xs font-semibold text-gray-600 
+                                uppercase tracking-wider">
                             Nome
                         </th>
                         <th
-                            class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                            class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 
+                                text-left text-xs font-semibold text-gray-600
+                                uppercase tracking-wider">
                             E-mail
                         </th>
                         <th
-                            class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                            class="px-5 py-3 border-b-2 border-gray-200 
+                                bg-gray-100 text-left text-xs font-semibold text-gray-600 
+                                uppercase tracking-wider">
                             Data de criação
                         </th>
                         <th
-                            class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                            class="px-5 py-3 border-b-2 border-gray-200 
+                                bg-gray-100 text-left text-xs font-semibold text-gray-600 
+                                uppercase tracking-wider">
                             Ações
                         </th>
                     </tr>
                 </thead>
                 <tbody>
-                    @forelse ($users as $user)
+                    @forelse ($users->items() as $user)
                         <tr>
                             <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                                 <div class="flex items-center">
@@ -98,4 +106,40 @@
             </table>
         </div>
     </div>  
+
+    <nav aria-label="Page navigation py-12">
+        <ul class="inline-flex -space-x-px mt-10">
+            @if ($users->currentPage() > 1)
+                <li>
+                    <a href="?page={{ $users->currentPage() - 1 }}" 
+                        class="py-2 px-3 ml-0 leading-tight text-gray-500 bg-white 
+                            rounded-l-lg border border-gray-300 hover:bg-gray-100 
+                            hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 
+                            dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                        Anterior
+                    </a>
+                </li>
+            @endif
+            <li>
+                <a href="#" aria-current="page" 
+                    class="py-2 px-3 text-blue-600 bg-blue-50 border 
+                    border-gray-300 hover:bg-blue-100 hover:text-blue-700 
+                    dark:border-gray-700 dark:bg-gray-700 dark:text-white">
+                    {{ $users->currentPage() }}
+                </a>
+            </li>
+            @if ($users->currentPage() < $users->lastPage())
+                <li>
+                    <a href="?page={{ $users->currentPage() + 1 }}" 
+                        class="py-2 px-3 leading-tight text-gray-500 bg-white
+                        rounded-r-lg border border-gray-300 hover:bg-gray-100 
+                        hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 
+                        dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                        Próxima
+                    </a>
+                </li>
+            @endif
+        </ul>
+    </nav>
+
 @endsection
