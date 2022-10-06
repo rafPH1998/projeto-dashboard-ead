4a3023dfd3784f7b6064c8169a92ad35c9b32bb7
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-10-05 22:19:28 -0300

    retornando um paginationPresenter para cursos

--- app/Repositories/CourseRepositoryInterface.php
+++ app/Repositories/CourseRepositoryInterface.php
@@ -2,11 +2,12 @@
 
 namespace App\Repositories;
 
+use App\Repositories\Presenters\PaginationPresenter;
 
 interface CourseRepositoryInterface
 {
 
-    public function getAll(string $filter = ''): array;
+    public function getAll(string $filter = ''): PaginationPresenter;
     public function findById(string $id): object|null;
     public function update(string $id, array $data): object|null;
     public function delete(string $id): bool;
--- app/Repositories/Eloquent/CourseRepository.php
+++ app/Repositories/Eloquent/CourseRepository.php
@@ -4,6 +4,7 @@ namespace App\Repositories\Eloquent;
 
 use App\Models\Course;
 use App\Repositories\CourseRepositoryInterface;
+use App\Repositories\Presenters\PaginationPresenter;
 
 class CourseRepository implements CourseRepositoryInterface
 {
@@ -13,7 +14,7 @@ class CourseRepository implements CourseRepositoryInterface
     ){}
 
 
-    public function getAll(string $filter = ''): array
+    public function getAll(string $filter = ''): PaginationPresenter
     {
         $courses = $this->course
                         ->where(function ($query) use ($filter) {
@@ -21,10 +22,12 @@ class CourseRepository implements CourseRepositoryInterface
                                 $query->Orwhere('name', 'LIKE', "%{$filter}%");
                             }
                         })
+                        ->whereDate('created_at', '>=', now()->subDays(4))     
+                        ->whereDate('created_at', '<=', now()->subDays(1))        
                         ->orderBy('id', 'DESC')
-                        ->get();
+                        ->paginate(3);
 
-        return $courses->toArray();
+        return new PaginationPresenter($courses);
     }
 
     public function findById(string $id): object|null
