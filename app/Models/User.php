9f9da649c8065fbfa43cc9e7581ced78e4073893
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-10-05 22:26:49 -0300

    fillable para cpf

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,47 +2,31 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Storage;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
+        'cpf',
         'image'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
+
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
@@ -65,6 +49,13 @@ class User extends Authenticatable
     {
         return Attribute::make(
             get: fn ($value) => $value ? Storage::url($value) : null
+        );
+    }
+    
+    protected function cpf(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $value ? format_document($value) : null
         );
     }
 }
