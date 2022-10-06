87015a5aa5da28540bc28f131cfd0cbb5c89c483
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-10-05 22:20:35 -0300

    funcao para formatação de cpf e cnpj

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -19,3 +19,22 @@ if (!function_exists('convertItemsOfArrayToObject')) {
     }
 }
 
+//valida cpf e cnpj
+
+if (!function_exists('format_document')) {
+
+    function format_document(string $cpfOrCnpj): string 
+    {
+        $CPF_LENGHT = 11;
+
+        $cnpj_cpf = preg_replace("/\D/", '', $cpfOrCnpj);
+
+        if (strlen($cnpj_cpf) === $CPF_LENGHT) {
+            return preg_replace("/(\d{3})(\d{3})(\d{3})(\d{2})/", "\$1.\$2.\$3-\$4", $cnpj_cpf);
+        }
+
+        return preg_replace("/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/", "\$1.\$2.\$3/\$4-\$5", $cnpj_cpf);
+    }
+}
+
+
