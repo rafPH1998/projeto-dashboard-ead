71d3df14d3d1f4404d4e828665b1259faac509f7
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-10-05 22:28:02 -0300

    exibindo CPNJ/CPF na view de users

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -36,6 +36,12 @@
                             E-mail
                         </th>
                         <th
+                            class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 
+                                text-left text-xs font-semibold text-gray-600
+                                uppercase tracking-wider">
+                            CPF/CNPJ
+                        </th>
+                        <th
                             class="px-5 py-3 border-b-2 border-gray-200 
                                 bg-gray-100 text-left text-xs font-semibold text-gray-600 
                                 uppercase tracking-wider">
@@ -55,7 +61,8 @@
                             <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                                 <div class="flex items-center">
                                     <div class="flex-shrink-0 w-10 h-10">
-                                        <img class="w-full h-full rounded-full" src="{{ $user->image ? $user->image : url('images/user.png') }}">
+                                        <img class="w-full h-full rounded-full" 
+                                            src="{{ $user->image ? $user->image : url('images/user.png') }}">
                                     </div>
                                     <div class="ml-3">
                                         <p class="text-gray-900 whitespace-no-wrap">
@@ -67,6 +74,11 @@
                             <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                                 <p class="text-gray-900 whitespace-no-wrap">
                                     {{$user->email}}
+                                </p>
+                            </td>
+                            <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                <p class="text-gray-900 whitespace-no-wrap">
+                                    {{$user->cpf}}
                                 </p>
                             </td>
                             <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
