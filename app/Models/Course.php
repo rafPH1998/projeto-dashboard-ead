4b8dec17c18402a6cf30e61d1316aece7537fb5f
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-10-05 21:38:33 -0300

    mutator para created_at

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -22,6 +23,13 @@ class Course extends Model
         return $this->hasMany(Module::class);
     }
 
+    protected function createdAt(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => Carbon::make($value)->format('d/m/Y')
+        );
+    }
+    
     protected function image(): Attribute
     {
         return Attribute::make(
