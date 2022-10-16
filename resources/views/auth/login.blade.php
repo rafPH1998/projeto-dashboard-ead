e3d6e4c58ff690e27485f97e373c23a8b03b4e94
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-10-16 13:58:48 -0300

    Botao (Login com o google)

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -52,5 +52,16 @@
                 </x-button>
             </div>
         </form>
+
+        <a href="{{ route('login.google') }}" class="inline-flex items-center px-4 
+            py-2 bg-gray-800 border border-transparent 
+            rounded-md font-semibold text-xs text-white 
+            uppercase tracking-widest hover:bg-gray-700 
+            active:bg-gray-900 focus:outline-none focus:border-gray-900 
+            focus:ring ring-gray-300 disabled:opacity-25 transition 
+            ease-in-out duration-150">
+            Login com o google
+        </a>
+
     </x-auth-card>
 </x-guest-layout>
