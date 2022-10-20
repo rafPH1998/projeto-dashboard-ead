644674580f6ad5d0c2fbd7e2a67b872424494fa4
Rafael Belchior <rafaelb89.rb@gmail.com>
2022-10-20 15:45:51 -0300

    Traduzindo as palavras de texto do botãp

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -24,7 +24,7 @@
 
             <!-- Password -->
             <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
+                <x-label for="password" :value="__('Senha')" />
 
                 <x-input id="password" class="block mt-1 w-full"
                                 type="password"
@@ -33,25 +33,40 @@
             </div>
 
             <!-- Remember Me -->
-            <div class="block mt-4">
+            {{-- <div class="block mt-4">
                 <label for="remember_me" class="inline-flex items-center">
                     <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
                     <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                 </label>
-            </div>
+            </div> --}}
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
+                        {{ __('Esqueceu sua senha?') }}
                     </a>
                 @endif
 
                 <x-button class="ml-3">
-                    {{ __('Log in') }}
+                    {{ __('Login') }}
                 </x-button>
             </div>
         </form>
+
+        <a
+            href="{{route('register')}}"
+            type="button"
+            class="mt-4 inline-flex items-center px-4 
+            py-2 bg-gray-800 border border-transparent 
+            rounded-md font-semibold text-xs text-white 
+            uppercase tracking-widest hover:bg-gray-700 
+            active:bg-gray-900 focus:outline-none focus:border-gray-900 
+            focus:ring ring-gray-300 disabled:opacity-25 transition 
+            ease-in-out duration-150"
+            data-mdb-ripple="true"
+            data-mdb-ripple-color="light">
+            <p class="ml-2">Cadastrar</p>
+        </a><br/>
 
         <a href="{{ route('auth.social.redirect', ['driver' => 'google'])  }}" class="mt-4 inline-flex items-center px-4 
             py-2 bg-gray-800 border border-transparent 
